2d3bad8c1aff67ecc25b24a577e2efe29bf3f6ca
Leonardo martins qualharello <84809180+leomqua@users.noreply.github.com>
2024-09-26 18:26:34 -0300

    Add files via upload

---
+++ New Talents/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Talents
+{
+    public class Calculadora
+    {
+        private List<string> ListaHistorico;
+        private string data;
+        public Calculadora(string data)
+        {
+            ListaHistorico = new List<string>();
+            this.data = data;
+        }
+        public int Somar(int num1, int num2)
+        {
+            int res = num1 + num2;
+            ListaHistorico.Insert(0,"Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            int res = num1 - num2;
+            ListaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+
+            return res;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            int res = num1 * num2;
+            ListaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+
+            return res;
+        }
+        public int Dividir(int num1, int num2)
+        {
+            int res = num1 / num2;
+            ListaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            ListaHistorico.RemoveRange(3,ListaHistorico.Count - 3);
+            return ListaHistorico;
+        }
+    }
+}
